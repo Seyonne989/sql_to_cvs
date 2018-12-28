066b00a3eb87e5666b41d4f9d10ea1320aebe931
U-DESKTOP-BVLO11C\Yoni <yonidrori@gmail.com>
2018-12-28 14:52:25 +0200

    trying to send a line from variables

--- Testing.R
+++ Testing.R
@@ -11,11 +11,19 @@ con <- dbConnect(RMySQL::MySQL(),
 Testing <- read.csv(file = 'C:/Users/Yoni/Desktop/sql_to_cvs/sample.csv', header = FALSE)
 
 i <- 1
-while ( ! is.na( Testing[i,1]) ) {
-   print(Testing[i,2])
-   i = i + 1
- }
+#while ( ! is.na( Testing[i,1]) ) {
+#  print(Testing[i,2])
+# i = i + 1
+# }
 
 
-res <- dbSendQuery(con, "INSERT INTO `r_exp`.`import_csv` (`company_text`, `boss`, `ranking`, `popularity`, `bonus`, `sick_days`,`vacation`, `location`,`department`,`weight`) 
-VALUES ('EA', 'Jon', '999', '-1', '0', '1000','6','Isreal','paper','0.4');")
+#res <- dbSendQuery(con, "INSERT INTO `r_exp`.`import_csv` (`company_text`, `boss`, `ranking`, `popularity`, `bonus`, `sick_days`,`vacation`, `location`,`department`,`weight`) 
+#VALUES ('EA', 'Jon', '999', '-1', '0', '1000','6','Isreal','paper','0.4');")
+sqlstr <- "INSERT INTO `r_exp`.`import_csv` (`company_text`, `boss`, `ranking`, `popularity`, `bonus`, `sick_days`,`vacation`, `location`,`department`,`weight`)"
+sqlstr <- paste(sqlstr, "VALUES", sep = " ")
+sqlstr <- paste(sqlstr,  Testing[i,2], sep = " '")
+sqlstr <- paste(sqlstr,  ",", sep = " '")
+sqlstr <- paste(sqlstr,  Testing[i,3], sep = " '")
+sqlstr <- paste(sqlstr,  ",", sep = " '")
+sqlstr <- paste(sqlstr,  Testing[i,4], sep = " '")
+print(sqlstr) 
